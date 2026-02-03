167f9ceabae73449c11739a42dabad8698d86a1c
Michal Sitek <michal.sitek2306@gmail.com>
2026-02-03 23:20:33 +0100

    Added main file, settings and ship

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """class for storing settings"""
+
+    def __init__(self):
+        """game settings initialization"""
+        #screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 2.0
